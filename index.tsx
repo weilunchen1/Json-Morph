cedbcd3188895865622f23243ab25631a54e0537
weilunchen1 <weilunchen@91app.com>
2026-01-27 13:39:12 +0800

    refactor: Simplify application title
    Update the application title from "JSON Morph - 專業資料轉換工具" to "JSON Tools" and remove the tagline. This change simplifies the branding and title for broader appeal.

--- index.tsx
+++ index.tsx
@@ -197,9 +197,8 @@ const App: React.FC = () => {
           <span className="bg-indigo-600 p-2 rounded-lg text-white">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 7.7c0-.6-.4-1.2-.8-1.5l-6.3-3.9a1.72 1.72 0 0 0-1.7 0l-10.3 6c-.5.2-.9.8-.9 1.4v6.6c0 .5.4 1.2.8 1.5l6.3 3.9a1.72 1.72 0 0 0 1.7 0l10.3-6c.5-.3.9-1 .9-1.5Z"/><path d="M10 21.9V14L2.1 9.1"/><path d="m10 14 11.9-6.9"/><path d="M14 19.8v-8.1"/><path d="M18 17.5V9.4"/></svg>
           </span>
-          JSON Morph
+          JSON Tools
         </h1>
-        <p className="text-slate-400 font-medium">專業資料轉換器 - 16 格精密三行佈局</p>
       </header>
 
       {/* 第一行按鈕：4/4/4/4 */}
